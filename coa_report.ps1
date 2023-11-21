14f2daa478ab9a2a851470db3c57d7708402960c
Tyler Trice <45208554+Deltron2020@users.noreply.github.com>
2023-11-21 15:58:01 -0500

    Add files via upload

---
+++ coa_report.ps1
@@ -0,0 +1,15 @@
+﻿$path = "\\network_path\Change_Of_Address"
+$xlsFile = (Get-ChildItem -Path $path -force | Where-Object Extension -in ('.xls')).FullName
+$xlsCount = (Get-ChildItem -Path $path -force | Where-Object Extension -in ('.xls') | Measure-Object).Count
+
+if ($xlsCount -eq 1) {
+
+     py "\\network_path\ChangeOfAddress\main.py" $xlsFile
+
+ }
+
+else {
+
+    exit
+
+ }
